cf7a127e772294bc8279df895b158e39f9102fc6
Ejiro-Frances <francesejiro@yahoo.com>
2025-11-16 18:11:04 +0100

    feat(ui): update shadcn button and input components to match the figma design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,8 @@
-import React from 'react'
-import Form_input from '../_components/Form-Input'
+import React from "react"
+import Form_input from "../_components/Form-Input"
 
 const Login = () => {
-  return (
-     
-      
-      
-     <Form_input label={"Enter ID"} input_placeholder={"Enter your ID"} />
-    
-  )
+  return <Form_input label={"Enter ID"} input_placeholder={"Enter your ID"} />
 }
 
 export default Login
