2882d2cd75a457339eef10530e26ac451760fe57
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-11-17 10:01:31 +0100

    fix(lint): resolve lint issues with the waitlist implementation

--- src/app/waitlist/layout.tsx
+++ src/app/waitlist/layout.tsx
@@ -1,9 +1,7 @@
-import Footer from "./_components/waitlist-footer";
-import Navbar from "./_components/waitlist-navbar"
-
+import Footer from "./_components/waitlist-footer"
 export const metadata = {
   title: "Waitlist",
-};
+}
 
 export default function ExternalLayout({
   children,
@@ -12,7 +10,7 @@ export default function ExternalLayout({
 }>) {
   return (
     <>
-      <main className="pt-20 md:pt-22 lg:pt-28 bg-[#F5F5F5] px-0">{children}</main>
+      <main className="bg-[#F5F5F5] px-0 pt-20 md:pt-22 lg:pt-28">{children}</main>
       <Footer />
     </>
   )
