c68f6f8277a4744461f9b0deeb6b4fb025271af6
MhideTech <okefolahanolamide2006@gmail.com>
2025-11-18 10:56:39 +0100

    fix(waitlist): fixed eslint error

--- src/app/(waitlist)/_hooks/use-media-query.ts
+++ src/app/(waitlist)/_hooks/use-media-query.ts
@@ -4,14 +4,13 @@ export function useMediaQuery(query: string) {
   const [matches, setMatches] = useState(false)
 
   useEffect(() => {
-    const media = window.matchMedia(query)
-    if (media.matches !== matches) {
-      setMatches(media.matches)
-    }
-    const listener = () => setMatches(media.matches)
-    window.addEventListener("resize", listener)
-    return () => window.removeEventListener("resize", listener)
-  }, [matches, query])
+    const mediaQueryList = window.matchMedia(query)
+    const listener = () => setMatches(mediaQueryList.matches)
+
+    listener()
+    mediaQueryList.addEventListener("change", listener)
+    return () => mediaQueryList.removeEventListener("change", listener)
+  }, [query])
 
   return matches
 }
