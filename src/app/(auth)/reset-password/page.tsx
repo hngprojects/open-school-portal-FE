3cfba9ffe06bea7aba61d64e792956d91c488378
Ejiro-Frances <francesejiro@yahoo.com>
2025-11-19 20:00:43 +0100

    fix: fixed error in reset password page using suspense from react

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import PasswordResetForm from "../_components/password-reset"
 
 const ResetPasswordPage = () => {
-  return <PasswordResetForm />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PasswordResetForm />
+    </Suspense>
+  )
 }
 
 export default ResetPasswordPage
