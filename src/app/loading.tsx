038dbc16d333f8e450e15116816f21e859822677
David <duwagbale07@gmail.com>
2025-11-22 12:02:16 +0100

    fix: wrap layout in general query provider

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { Loader } from "lucide-react"
+import { GeneralQueryProvider } from "@/providers/general-query-provider"
 
 const Loading = () => {
   return (
+    <GeneralQueryProvider>
     <div className="flex min-h-screen w-full items-center justify-center bg-[#FEF9FA]/70">
       <Loader className="size-16 animate-spin" />
     </div>
+    </GeneralQueryProvider>
   )
 }
 
