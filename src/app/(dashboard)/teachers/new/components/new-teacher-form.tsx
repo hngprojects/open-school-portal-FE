6ecdf9d368740c86ae492d587b8d64c111c6020e
Lilianobi <obililian.n@gmail.com>
2025-11-23 13:30:26 +0100

    chore: remove unused variable to fix lint warning

--- src/app/(dashboard)/teachers/new/components/new-teacher-form.tsx
+++ src/app/(dashboard)/teachers/new/components/new-teacher-form.tsx
@@ -4,7 +4,7 @@ import {
   NewPersonFormBuilder,
   NewPersonFormConfig,
 } from "@/app/(dashboard)/_components/add-new-person-form-template"
-import { User } from "@/types/user"
+//import { User } from "@/types/user"
 //import { User } from "@/types/user"
 
 const generatePassword = () => {
@@ -118,9 +118,9 @@ export const teacherFormConfig: NewPersonFormConfig = {
 export default function NewTeacherForm() {
   return <NewPersonFormBuilder config={teacherFormConfig} onSubmit={handleSubmit} />
 
-  async function handleSubmit(formData: Record<string, unknown>) {
+  async function handleSubmit(/*_formData: Record<string, unknown>*/) {
     console.log("Submitting new teacher form...")
-    const firstName = formData.firstName as string
+    /* const firstName = formData.firstName as string
     const lastName = formData.lastName as string
     const id = formData.employmentId as string
     const title = formData.title as string
@@ -129,7 +129,7 @@ export default function NewTeacherForm() {
     const phoneNumber = formData.phoneNumber as string
     const homeAddress = formData.homeAddress as string
     const middleName = (formData.middleName as string) || ""
-
+*/
     /*const newTeacher: User = {
       id,
       name: `${title} ${firstName} ${lastName}`,
